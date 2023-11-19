96ebcd3787e774ff438ed12157d42588b192a5da
Helen Hua <helenhua999@gmail.com>
2023-11-19 14:19:24 -0800

    homepage + styling

--- frontend/src/modules/pages/homePage.jsx
+++ frontend/src/modules/pages/homePage.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 export default function HomePage() {
   return (
-    <div>Home</div>
+
+    
+    <div> 
+      <Container>
+        <Row className="body">
+          <Col className="imgCol">
+            <img src="./homepage.png" className="homepageImg">
+            </img> </Col>
+          <Col className="rightHalf">
+            <Row><h1>Get comfy and elevate your day in 5 minutes</h1></Row>
+            <Row> <button>Let's go!</button></Row>
+          </Col>
+        </Row>
+        
+      </Container>
+      
+      
+    </div>
   )
 }
