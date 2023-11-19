05e99683bf30b4c6ffb4bc43879756d568c02aa3
Helen Hua <helenhua999@gmail.com>
2023-11-19 10:18:19 -0800

    basic calendar implemented

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import 'app'
+import HomePage from "modules/pages/homePage"
 
 function App() {
   return (
     <div className="App">
-      <div>Test</div>    
+      <div>Test</div>  
+      <HomePage></HomePage>  
     </div>
   )
 }
---
+++ frontend/src/modules/pages/homePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Calendar from './calendar'
+
+export default function HomePage() {
+  return (
+    <div className="App">
+      <div>HomePage</div>  
+      <Calendar></Calendar>
+    </div>
+    
+    
+  )
+}
