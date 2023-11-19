ea2c5aaa55dc71f8ad8b49f753065ebe075fd04a
jamiema1 <jkmm.1024@gmail.com>
2023-11-19 09:04:10 -0800

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.json('This is the backend')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('Connected to backend')
+  console.log('Running on port: ' + process.env.PORT)
+})
